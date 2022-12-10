1d38d3c6b18a363c9a2c8d99681d3cb5f61abb7b
Ovkorz <pupkaszymon@gmail.com>
2022-12-10 23:05:47 +0100

    add simplex_NM algorithm implementation

--- source/opt_alg.cpp
+++ source/opt_alg.cpp
@@ -1,4 +1,6 @@
 #include"opt_alg.h"
+#include"user_funs.h"
+
 long double* expansion(long double(*ff)(long double), long double x0, long double d, double alpha, int Nmax, int &f_calls)
 {
 	try
@@ -155,6 +157,100 @@ solution sym_NM(matrix(*ff)(matrix, matrix, matrix), matrix x0, double s, double
 	{
 		solution Xopt;
 		//Tu wpisz kod funkcji
+
+		int n = get_dim(x0);
+
+		matrix *simplex = new matrix[n+1];
+		simplex[0] = x0;
+		for(int i = 1; i <= n; i++){
+			double* temp = new double[n];
+			for(int j = 0; j < n; j++) temp[j] = 0;
+			
+			temp[i-1] = lambda;
+			simplex[i] = matrix(x0 + matrix(n, temp));
+		}
+
+		int max_index =0, min_index=0;
+		while(true){
+
+			// ######## set up of indexes ########
+			solution y_min(simplex[0]); y_min.fit_fun(ff, ud1, ud2);
+			solution y_max(simplex[0]); y_max.fit_fun(ff, ud1, ud2);
+			
+			for(int i = 1; i <= n; i++){
+				solution X(simplex[i]).fit_fun(ff, ud1, ud2);
+				if(X.y < y_min.y){
+					y_min.y = X.y;
+					min_index = i;
+				}
+				else if (X.y > y_max.y){
+					y_max.y = X.y;
+					max_index = i;
+				}
+			}
+			//#############################
+
+			//######## mid-point ########
+			matrix sum_filtered(0);
+			for(int i = 0; i <= n; i++){
+				if(i != max_index) sum_filtered += simplex[i];
+			}
+			matrix mid_point = sum_filtered / matrix(n);
+
+			//######## reflection ########
+			matrix refl_point = simplex[max_index] + alpha * (mid_point - simplex[max_index]);
+			solution y_refl(refl_point).fit_fun(ff, ud1, ud2);
+
+			//######## expansion ########
+			if(y_refl.y < y_min.y){
+				matrix exp_point(mid_point + delta * (refl_point - mid_point));
+				solution y_exp(exp_point).fit_fun(ff, ud1, ud2);
+				if(y_exp.y < y_refl.y) 
+					simplex[max_index] = exp_point;
+
+				else
+					simplex[max_index] = refl_point;				
+			}
+			//######## reflection acceptance ########
+			else if(y_refl.y < y_max.y)
+				simplex[max_index] = refl_point;
+
+			//######## contraction ########
+			else {
+				matrix contr_point = mid_point + beta * (simplex[max_index] - mid_point);
+				solution y_contr(contr_point).fit_fun(ff, ud1, ud2);
+				if(y_contr.y < y_exp.y)
+					simplex[max_index] = contr_point;
+
+			//######## reduction ########
+				else{
+					for(int i = 0; i <=n; i++){
+						if(i == min_index) continue;
+						else{
+							simplex[i] = delta * (simplex[i] + simplex[min_index]);
+						}
+					}
+				}
+			}
+
+			if(solution.f_calls > Nmax)
+				throw "Too many calls.";
+
+			bool stop_condition = false;
+			for(int i = 0; i<=n; i++){
+				if(geometric_mean(simplex[i] - simplex[min_index]) < epsilon){
+					stop_condition = true;
+					break;
+				}
+			}
+
+			if(stop_condition){
+				Xopt.x = simplex[min_index];
+				x.fit_fun(ff, ud1, ud2);
+				break;
+			}
+
+		}
 
 		return Xopt;
 	}
