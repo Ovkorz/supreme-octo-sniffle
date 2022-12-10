a1c8d9f3abdf3f28adcce7a801445e08e854c908
Ovkorz <pupkaszymon@gmail.com>
2022-12-10 23:05:30 +0100

    add geometric_mean user function

--- include/user_funs.h
+++ include/user_funs.h
@@ -2,4 +2,4 @@
 
 #include"ode_solver.h"
 
-
+double geometric_mean (const matrix &A);
--- source/user_funs.cpp
+++ source/user_funs.cpp
@@ -1,2 +1,13 @@
 #include"user_funs.h"
+#include<cmath>
 
+double geometric_mean (const matrix &A){
+    double sum_of_squares = 0;
+    int n = get_dim(A);
+
+    for(int i = 0; i<n; i++){
+        sum_of_squares += pow(A[i], 2);
+    }
+
+    return sqrt(sum_of_squares);
+}
